a921b904e8b2f4c71ff63da98d52d8331d73c210
Joe Mackle <20073322+joe-mac-design@users.noreply.github.com>
2023-05-01 16:34:48 +0100

    Full Flow Implemented
    + Added profile customization sub menus
    + Added post game menu/restart button

--- Honours Project/Assets/Scripts/UIController.cs
+++ Honours Project/Assets/Scripts/UIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Threading.Tasks;
 using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
@@ -32,15 +33,31 @@ public class UIController : MonoBehaviour
 
     // Profile Menu
     public Button profileButton;
+    public Button IconButton;
+    public Button IconSaveButton;
+    public Button BadgesButton;
+    public Button NicknameButton;
+    public Button NicknameSaveButton;
+    public Button LessonCompleteButton;
+    public Button RestartButton;
+
+    [SerializeField] private string[] _ProfileMenuSaveButtonTextArray;
 
     // Visual Elements
     public VisualElement playMenu;
     public VisualElement optionsMenu;
     public VisualElement quitMenu;
     public VisualElement profileMenu;
+    public VisualElement ProfileMenuOptionsContainer;
+    public VisualElement IconContainer;
+    public VisualElement BadgesContainer;
+    public VisualElement NicknameContainer;
+    public VisualElement PostGameContainer;
+    public VisualElement LaunchContainer;
 
     public VisualElement MainContainer;
     public VisualElement GameContainer;
+    public VisualElement LessonContainer;
 
     // Start is called before the first frame update
     void Start()
@@ -49,6 +66,7 @@ public class UIController : MonoBehaviour
 
         MainContainer = root.Q<VisualElement>("MainContainer");
         GameContainer = root.Q<VisualElement>("GameContainer");
+        LaunchContainer = root.Q<VisualElement>("LaunchContainer");
 
         playButton = root.Q<Button>("PlayButton");
         optionsButton = root.Q<Button>("OptionsButton");
@@ -64,6 +82,24 @@ public class UIController : MonoBehaviour
         playMenu = root.Q<VisualElement>("PlayMenuContainer");
         profileMenu = root.Q<VisualElement>("ProfileMenuContainer");
 
+        ProfileMenuOptionsContainer = root.Q<VisualElement>("ProfileOptionsContainer");
+        IconContainer = root.Q<VisualElement>("IconContainer");
+        BadgesContainer = root.Q<VisualElement>("BadgesContainer");
+        NicknameContainer = root.Q<VisualElement>("NicknameContainer");
+
+        LessonContainer = root.Q<VisualElement>("LessonCompleteContainer");
+        LessonCompleteButton = root.Q<Button>("GameCompleteContinueButton");
+
+        RestartButton = root.Q<Button>("RestartBtn");
+
+        IconButton = root.Q<Button>("IconBtn");
+        IconSaveButton = root.Q<Button>("IconSaveButton");
+
+        BadgesButton = root.Q<Button>("BadgesBtn");
+
+        NicknameButton = root.Q<Button>("NicknameBtn");
+        NicknameSaveButton = root.Q<Button>("NicknameSaveButton");
+
         StaticMap = root.Q<VisualElement>("StaticMapContainer");
         StaticNAButton = root.Q<Button>("StaticNA");
         ActiveMap = root.Q<VisualElement>("ActiveMapContainer");
@@ -72,6 +108,8 @@ public class UIController : MonoBehaviour
         MapPointer_1 = root.Q<Button>("MapPointer-1");
         LessonBox_1 = root.Q<VisualElement>("LessonBox-1");
         LessonBoxButton_1 = root.Q<Button>("LessonStartButton");
+
+        PostGameContainer = root.Q<VisualElement>("PostGameContainer");
 
         MapPointer_2 = root.Q<Button>("MapPointer-2");
         LessonBox_2 = root.Q<VisualElement>("LessonBox-2");
@@ -83,6 +121,18 @@ public class UIController : MonoBehaviour
         quitButton.clicked += QuitButtonActive;
         playButton.clicked += PlayButtonActive;
         profileButton.clicked += ProfileButtonActive;
+
+        IconButton.clicked += IconContainerActive;
+        IconSaveButton.clicked += IconSaveButtonActive;
+
+        BadgesButton.clicked += BadgesContainerActive;
+
+        NicknameButton.clicked += NicknameContainerActive;
+        NicknameSaveButton.clicked += NicknameSaveButtonActive;
+
+        LessonCompleteButton.clicked += LessonCompleteButtonActive;
+
+        RestartButton.clicked += RestartButtonActive;
 
         quitConfirmButton.clicked += QuitConfirmActive;
         quitNegativeButton.clicked += QuitNegativeActive;
@@ -135,9 +185,17 @@ public class UIController : MonoBehaviour
 
     void ProfileButtonActive()
     {
-        profileMenu.style.display = DisplayStyle.Flex;
-        quitMenu.style.display = DisplayStyle.None;
-        optionsMenu.style.display = DisplayStyle.None;
+        // If the ProfileButton is clicked, the ProfileMenu will be enabled, if it is clicked again, it will be disabled
+        if (profileMenu.style.display == DisplayStyle.None)
+        {
+            profileMenu.style.display = DisplayStyle.Flex;
+            quitMenu.style.display = DisplayStyle.None;
+            optionsMenu.style.display = DisplayStyle.None;
+        }
+        else
+        {
+            profileMenu.style.display = DisplayStyle.None;
+        }
     }
 
     void ActiveMapActive()
@@ -199,4 +257,62 @@ public class UIController : MonoBehaviour
             LessonBox_3.visible = false;
         }
     }
-}
+
+    void IconContainerActive() 
+    {
+        IconContainer.style.display = DisplayStyle.Flex;
+        BadgesContainer.style.display = DisplayStyle.None;
+        NicknameContainer.style.display = DisplayStyle.None;
+    }
+
+    async void IconSaveButtonActive() 
+    {
+        IconSaveButton.text = _ProfileMenuSaveButtonTextArray[0];
+        await Task.Delay(1000);
+        IconSaveButton.text = _ProfileMenuSaveButtonTextArray[1];
+        await Task.Delay(1000);
+        IconSaveButton.text = _ProfileMenuSaveButtonTextArray[2];
+        await Task.Delay(1000);
+        IconSaveButton.text = _ProfileMenuSaveButtonTextArray[3];
+    }
+
+    void BadgesContainerActive()
+    {
+        IconContainer.style.display = DisplayStyle.None;
+        BadgesContainer.style.display = DisplayStyle.Flex;
+        NicknameContainer.style.display = DisplayStyle.None;
+    }
+
+    void NicknameContainerActive()
+    {
+        IconContainer.style.display = DisplayStyle.None;
+        BadgesContainer.style.display = DisplayStyle.None;
+        NicknameContainer.style.display = DisplayStyle.Flex;
+    }
+
+    async void NicknameSaveButtonActive() 
+    {
+        NicknameSaveButton.text = _ProfileMenuSaveButtonTextArray[0];
+        await Task.Delay(1000);
+        NicknameSaveButton.text = _ProfileMenuSaveButtonTextArray[1];
+        await Task.Delay(1000);
+        NicknameSaveButton.text = _ProfileMenuSaveButtonTextArray[2];
+        await Task.Delay(1000);
+        NicknameSaveButton.text = _ProfileMenuSaveButtonTextArray[3];
+    }
+
+    void LessonCompleteButtonActive()
+    {
+        LessonContainer.style.display = DisplayStyle.None;
+        PostGameContainer.style.display = DisplayStyle.Flex;
+    }
+
+    void RestartButtonActive()
+    {
+        // quit the game or quit the editor
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+ }
